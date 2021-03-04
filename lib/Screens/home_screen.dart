9f100dd5f241af348e086f5ee5c008ce1c3d8b7d
admin <admin@admins-MacBook-Pro.local>
2021-03-04 20:54:50 +0530

    Grid View Added

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:neostore_app/Screens/login.dart';
 import 'package:neostore_app/bloc/login_bloc.dart';
 import 'package:neostore_app/usermodel.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 class HomeScreen extends StatefulWidget {
   @override
   _HomeScreenState createState() => _HomeScreenState();
@@ -11,22 +12,24 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   Color myHexColor1 = Color(0xfffe3f3f);
   Color myHexColor = Color(0xffe91c1a);
-  final drawObj= LoginBloc();
+  final data=LoginBloc();
   @override
   Widget build(BuildContext context) {
+    Size size=MediaQuery.of(context).size;
+    final appBar=AppBar(
+      title: Text('NeoSTORE'),
+      centerTitle: true,
+      elevation: 0.0,
+      backgroundColor:myHexColor,
+      actions: <Widget>[
+        IconButton(
+          onPressed: () {},
+          icon:Icon(Icons.search),
+        ),
+      ],
+    );
     return Scaffold(
-      appBar: AppBar(
-        title: Text('NeoSTORE'),
-        centerTitle: true,
-        elevation: 0.0,
-        backgroundColor:myHexColor,
-        actions: <Widget>[
-          IconButton(
-            onPressed: () {},
-            icon:Icon(Icons.search),
-          ),
-        ],
-      ),
+      appBar:appBar,
       drawer: Drawer(
         child: Container(
           color: Colors.grey[850],
@@ -39,12 +42,7 @@ class _HomeScreenState extends State<HomeScreen> {
                   data:ThemeData.light().copyWith(
                       primaryColor: Colors.grey[850],
                   ),
-                    child:StreamBuilder<UserData>(
-                      stream:drawObj.drawStream,
-                      builder: (context,snapshot) {
-                        print('bolte ${snapshot.data.firstName}');
-                        if(snapshot.data!=null) {
-                          return Column(
+                    child:Column(
                             children: [
                               SizedBox(height: 37.0),
                               Container(
@@ -64,11 +62,7 @@ class _HomeScreenState extends State<HomeScreen> {
                               Text("kinjal.jain@wwindia.com", style: TextStyle(
                                   fontSize: 13.0, color: Colors.white),)
                             ],
-                          );
-                        }
-                        return Text('');
-                      }
-                    )
+                          )
               ),
               SizedBox(height:13.0),
               Divider(color: Colors.black,height: 2.0,),
@@ -139,22 +133,128 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ),
       ),
-      body:ListView(
+      body:Column(
         children: [
-          SizedBox(
-            height: 228.0,
-            child: Carousel(
-              dotColor:myHexColor1 ,
-              dotSpacing: 20.0,
-              images: [
-                Image.network('https://images.unsplash.com/photo-1499933374294-4584851497cc?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTN8fHRhYmxlfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',fit: BoxFit.cover),
-                Image.network('https://images.unsplash.com/photo-1540574163026-643ea20ade25?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8c29mYXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',fit: BoxFit.cover),
-                Image.network('https://images.unsplash.com/photo-1584553193958-1e13dda62d3c?ixid=MXwxMjA3fDB8MHxzZWFyY2h8OHx8Y2hhaXJ8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',fit: BoxFit.cover),
-                Image.network('https://images.unsplash.com/photo-1591964173029-151a161dd66a?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MzN8fGN1cGJvYXJkfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',fit: BoxFit.cover),
-              ],
+              SizedBox(
+                height:(size.height-appBar.preferredSize.height)*0.44, //228.0,
+                child: Carousel(
+                  dotBgColor: Colors.transparent,
+                  dotColor:myHexColor1 ,
+                  dotSpacing: 20.0,
+                  images: [
+                    Image.network('https://images.unsplash.com/photo-1499933374294-4584851497cc?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTN8fHRhYmxlfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',fit: BoxFit.cover),
+                    Image.network('https://images.unsplash.com/photo-1540574163026-643ea20ade25?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8c29mYXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',fit: BoxFit.cover),
+                    Image.network('https://images.unsplash.com/photo-1584553193958-1e13dda62d3c?ixid=MXwxMjA3fDB8MHxzZWFyY2h8OHx8Y2hhaXJ8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',fit: BoxFit.cover),
+                    Image.network('https://images.unsplash.com/photo-1591964173029-151a161dd66a?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MzN8fGN1cGJvYXJkfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',fit: BoxFit.cover),
+                  ],
+                ),
+              ),
+          SizedBox(height:(size.height-appBar.preferredSize.height)*0.01), //15),
+          Expanded(
+            child: Container(
+              height:(size.height-appBar.preferredSize.height)*0.55,
+              child: GridView(
+                  gridDelegate:SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+              children: [
+                Container(
+                  color: myHexColor1,
+                  margin: EdgeInsets.fromLTRB(13,15,5.5,5.5),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Text('Tables',style: TextStyle(fontSize: 23,color: Colors.white),),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Icon(Icons.deck,color: Colors.white,size: 84.0),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  color: myHexColor1,
+                  margin: EdgeInsets.fromLTRB(5.5,15,13,5.5),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Icon(Icons.weekend,color: Colors.white,size: 84.0),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Text('Sofas',style: TextStyle(fontSize: 23,color: Colors.white),),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  color: myHexColor1,
+                  margin: EdgeInsets.fromLTRB(13,5.5,5.5,15),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Text('Chairs',style: TextStyle(fontSize: 23,color: Colors.white),),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Icon(Icons.event_seat,color: Colors.white,size: 84.0),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  color: myHexColor1,
+                  margin: EdgeInsets.fromLTRB(5.5,5.5,13,15),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Icon(Icons.table_chart_sharp,color: Colors.white,size: 84.0),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Text('Cupboards',style: TextStyle(fontSize: 23,color: Colors.white),),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],),
             ),
-          ),
-          Text(drawObj.firstName,style: TextStyle(color: Colors.black),),
+          )
         ],
       ),
     ) ;
--- lib/bloc/login_bloc.dart
+++ lib/bloc/login_bloc.dart
@@ -2,17 +2,18 @@ import 'dart:async';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:neostore_app/usermodel.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LoginBloc{
   final stateStreamController=StreamController<String>.broadcast();//broadcast use while login navigation since showing error
   var statusCode,firstName;
+  String key1,key2,key3,key4,get1,get2,get3,get4;
   StreamSink<String> get loginSink =>stateStreamController.sink;
   Stream<String> get loginStream =>stateStreamController.stream;
 
   final drawStreamController=StreamController<UserData>.broadcast();//broadcast use while login navigation since showing error
   StreamSink<UserData> get drawSink =>drawStreamController.sink;
   Stream<UserData> get drawStream =>drawStreamController.stream;
-
 
   postData(String email,String password) async {
     final String url = 'http://staging.php-dev.in:8844/trainingapp/api/users/login';
@@ -27,10 +28,18 @@ class LoginBloc{
       print(email);
       var success =SuccessModel.fromJson(json.decode(response.body));
       print(success.userMsg);
+      SharedPreferences prefs= await SharedPreferences.getInstance();
+      prefs.setString(key1,success.data.firstName);
+      prefs.setString(key2,success.data.lastName);
+      prefs.setString(key3,success.data.email);
+      prefs.setString(key4,success.data.profilePic);
+       get1 = prefs.getString(key1);
+       get2 = prefs.getString(key2);
+       get3 = prefs.getString(key3);
+       get4 = prefs.getString(key4);
       print('mohit ${success.data.firstName}');
-      firstName=success.data.firstName;
+      print('rohit ${get1}');
       loginSink.add(success.userMsg);
-       drawSink.add(success.data);
     }
     else if(response.statusCode==401){
       statusCode=response.statusCode;
